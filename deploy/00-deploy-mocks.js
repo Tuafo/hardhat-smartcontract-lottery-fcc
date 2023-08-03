343866fb66f97ff3189a5831a02088de55dc0d78
Tuafo <thiagomacenogoulart@gmail.com>
2023-08-02 21:56:09 -0300

    initial commit

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,13 +1,25 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. It costs 0.25 LINK per request
+    const GAS_PRICE_LINK = 1e9 // LINK per gas. calculated value based on the gas price of the chain.
 
     if (developmentChains.includes(network.name)) {
         log("Local network detected! Deploying Mocks...")
         // deploy a mock vrfcoordinator...
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+        log("Mocks Deployed!")
+        log("-----------------------------------------------------")
     }
 }
+module.exports.tags = ["all", "mocks"]
